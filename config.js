cf74fa1f9b4418923d9fb46dc61a479924980bfb
TYC <micarglss@gmail.com>
2025-03-05 17:01:33 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js - 包含實際密鑰，不上傳到 GitHub
+     const firebaseConfig = {
+              apiKey: "AIzaSyDWDDxsvDSuGlSOgqpk9BzZm0BBQlWqmYo",
+              authDomain: "updatephoto-77b73.firebaseapp.com",
+              projectId: "updatephoto-77b73",
+              storageBucket: "updatephoto-77b73.firebasestorage.app",
+              messagingSenderId: "352063991809",
+              appId: "1:352063991809:web:45442ef31dd8bbac571000"
+              };
